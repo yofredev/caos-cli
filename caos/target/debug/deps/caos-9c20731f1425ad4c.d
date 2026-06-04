3fec25be1591ce7ab2d9d3a1539a07e70b1a77f2
Yofre <yofrevelasquezqp@gmail.com>
2026-06-03 21:41:48 -0400

    feat: initial commit - caos is born

---
+++ caos/target/debug/deps/caos-9c20731f1425ad4c.d
@@ -0,0 +1,5 @@
+/home/yofredev/workspace/labs/caos/target/debug/deps/caos-9c20731f1425ad4c.d: src/main.rs
+
+/home/yofredev/workspace/labs/caos/target/debug/deps/libcaos-9c20731f1425ad4c.rmeta: src/main.rs
+
+src/main.rs:
